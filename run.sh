ad56d03408f8983d32500a11e90ac9c9e4f70e3c
ravalierukulla <31955341+ravalierukulla@users.noreply.github.com>
2020-07-26 21:46:59 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@ set -exuo pipefail
 apt-get update
 apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 
 add-apt-repository \
-"deb [arch=amd64] https://download.docker.com/linux/debian \
+"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) \
 stable"
 
